366551fda69e0846db2e64461076b701c00c7997
jeong-mincheol <jmc5371@nate.com>
2021-02-04 16:33:38 +0900

    반복문

---
+++ Java_006/src/com/callor/varriable/Loop_05.java
@@ -0,0 +1,27 @@
+package com.callor.varriable;
+
+public class Loop_05 {
+	
+	public static void main(String[] args) {
+		
+		int intDan = 7;
+		for(int num = 1 ; num < 10 ; num += 1) {
+			
+		}
+		for(int num = 1 ; num < 100 ; num += 1) {
+			// 변수가 100보다 작게 될 때 99번 반복되는 명령문이 된다			
+		}
+		for(int num = 20 ; num < 42 ; num += 1) {
+			System.out.println("안녕");
+		}
+		//  시작하는 값    비교하는 값   증가하는 값
+		for(int num = 12 ; num <= 20 ; num += 1) {
+			System.out.println("반가워");
+		}
+		for(int num = 12 ; num <= 20 ; num+= 2) {
+			System.out.println(num);
+		}
+ 
+	}
+
+}
