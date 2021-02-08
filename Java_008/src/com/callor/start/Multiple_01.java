a41e679f63a4c806100bb59982331c6672544b04
jeong-mincheol <jmc5371@nate.com>
2021-02-08 16:44:04 +0900

    class

--- Java_008/src/com/callor/start/Multiple_01.java
+++ Java_008/src/com/callor/start/Multiple_01.java
@@ -8,11 +8,14 @@ public class Multiple_01 {
 		for (int i = 0; i < 100; i++) {
 			int num = i + 7;
 			boolean bYes = num % 3 == 0;
+			
+			// if(num % 3 == 0)
+			// if(bYes == true)
 			if (bYes) {
-				System.out.println(num);
+				System.out.println(num + "는 3의배수");
 				sum += num;
 			}
-		}
-		System.out.println(sum);
+		} //for end 
+		System.out.println("3의 배수합 : " + sum);
 	}
 }
---
+++ Java_008/src/com/callor/start/bool/BoolAlge_02.java
@@ -0,0 +1,39 @@
+package com.callor.start.bool;
+
+public class BoolAlge_02 {
+	
+	public static void main(String[] args) {
+		
+		boolean bYes1 = 3 > 3;
+		boolean bYes2 = 3==3;
+		
+		// false && true == false
+		boolean bYes3 = bYes1 && bYes2;
+		
+		// false || true == true
+		boolean bYes4 = bYes1 || bYes2;
+		
+		int num1 = 100;
+		int num2 = 200;
+		
+		// (num > 50)의 결과가 true 이므로
+		// java는 효율적인 코드 실행을 위하여
+		// || 이후의 코드를 무시한다.
+		boolean bYes5 = (num1 > 50) || (num2 += 20) > 0; 
+		
+		if(num1 > 50 ) {
+			num2 += 20;
+		}
+		// num1 < 50이 false이므로
+		// && 이후의 코드는 무시한다.
+		bYes5 = (num1 < 50) && (num2 += 20) < 300;
+		    System.out.println();
+		
+		if( num1 < 50) {
+			num2 += 20;
+		}
+		
+		
+		
+	}
+}
