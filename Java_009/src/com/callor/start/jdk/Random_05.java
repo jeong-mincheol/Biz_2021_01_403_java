0ea67d654a03e582efd12a6c257dc4ca99a151ff
jeong-mincheol <jmc5371@nate.com>
2021-02-09 13:06:35 +0900

    Random

---
+++ Java_009/src/com/callor/start/jdk/Random_05.java
@@ -0,0 +1,22 @@
+package com.callor.start.jdk;
+
+import java.util.Random;
+
+public class Random_05 {
+	
+	public static void main(String[] args) {
+		
+		Random rnd = new Random(); 
+		
+		int sum = 0;
+		for(int i = 0 ; i < 100 ; i++) {
+			int num = rnd.nextInt(100) + 1;
+			System.out.println(num);
+			if(num % 2 == 0) {
+				sum += num;
+			}
+		}
+		System.out.println(sum);
+	}
+
+}
