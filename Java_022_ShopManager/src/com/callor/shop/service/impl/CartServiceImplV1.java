236c6f9f888303f23bbdb47ee6b2df66bd9dc8bc
jeong-mincheol <jmc5371@nate.com>
2021-03-04 16:33:46 +0900

    StringArray

--- Java_022_ShopManager/src/com/callor/shop/service/impl/CartServiceImplV1.java
+++ Java_022_ShopManager/src/com/callor/shop/service/impl/CartServiceImplV1.java
@@ -10,7 +10,24 @@ import com.callor.shop.values.Values;
 
 public class CartServiceImplV1 implements CartService{
 	
-	private List<CartVO> cartList;
+	/*
+	 * ImplV1에서 inputCart() method를 사용하여
+	 * cartList에 상품리스트를 추가하고 
+	 * 
+	 * ImplV1을 상속받은 ImplV2의 
+	 * 새로운 save...() method에서
+	 * cartList에 저장된 데이터를 사용하려고 시도했다
+	 * 
+	 * 이런한 경우에
+	 * private으로 선언된 변수(객체)는 서로 공유가 되지 않는다
+	 * 상속받은 클래스의 method에서 변수(객체)를 
+	 * 		공유하려면 부모 클래스에서
+	 * 		변수(객체)를 반드시 protected로 선언해 주어야 한다
+	 * 또한 상속받은 클래스에서는 해당 변수(객체)를 
+	 * 		선언해서는 안된다.
+	 */
+	protected List<CartVO> cartList;
+	
 	private final Scanner scan;
 	public CartServiceImplV1() {
 		scan = new Scanner(System.in);
@@ -28,13 +45,13 @@ public class CartServiceImplV1 implements CartService{
 		String strUserName = scan.nextLine();
 		
 		System.out.println("추가할 상품이름을 입력하세요");
-		System.out.println(">> ");
+		System.out.print(">> ");
 		String strProductNmae = scan.nextLine();
 		
 		System.out.println("추가할 상품의 구매 수량을 입력하세요");
 		Integer intQty = 0;
 		while(true) {
-			System.out.println(">> ");
+			System.out.print(">> ");
 			String strQty = scan.nextLine();
 			try {
 				intQty = Integer.valueOf(strQty);
@@ -48,9 +65,10 @@ public class CartServiceImplV1 implements CartService{
 			}
 		}
 
+		System.out.println("추가할 상품의 구매 가격을 입력하세요");
 		Integer intPrice = 0;
 		while(true) {
-			System.out.println(">> ");
+			System.out.print(">> ");
 			String strPrice = scan.nextLine();
 			try {
 				intPrice = Integer.valueOf(strPrice);
@@ -82,7 +100,8 @@ public class CartServiceImplV1 implements CartService{
 		System.out.println(Values.dLine);
 		System.out.println("추가할까요(YES)?");
 		String strYesNo = scan.nextLine();
-		if(strYesNo.equals("Yes")) {
+		if(strYesNo.equals("YES")) {
+			// 카트리스트에 cartVO를 만들어 계속 추가
 			cartList.add(cartVO);
 			System.out.println("저장완료!!");
 		} else {
@@ -91,15 +110,66 @@ public class CartServiceImplV1 implements CartService{
 		
 	}
 
+	/*
+	 * private으로 선언된 method는 
+	 * 현재 클래스에서만 호출이 가능한 method
+	 */
+	private void printHeader() {
+		System.out.println(Values.dLine);
+		System.out.println("구매자\t상품명\t단가\t수량\t금액");
+		System.out.println(Values.sLine);
+	}
+	// cartVO를 매개변수로 받는다
+	private void printBody(CartVO cartVO) {
+		// cartVO에서 각 인스턴스 변수를 읽어서 출력
+		System.out.print(cartVO.getUserName() + "\t");
+		System.out.printf("%s\t", cartVO.getProductName());
+		System.out.printf("%d\t", cartVO.getQty());
+		System.out.printf("%d\t", cartVO.getPrice());
+		System.out.printf("%d\n", cartVO.getTotal());
+	}
+	
+	
 	@Override
 	public void printAllCart() {
-		// TODO 장바구니 전체 보기
 		
+		this.printHeader();
+		
+		// 카트리스트의 상품개수 계산하여 nSize에 저장
+		int nSize = cartList.size();
+		for(int i = 0 ; i < nSize ; i++) {
+			/*
+			 * cartList.get(i)는
+			 * cartList에 저장되어있는 i번째 요소
+			 * 이 요소는 결국 CartVO 클래스로 만든 객체 
+			 */
+			/* System.out.print(cartList.get(i).getUserName() + "\t");
+			// 카트리스트에 있는 i번째 요소(=cartVO)를 꺼내서 
+			System.out.print(cartList.get(i).getProductName() + "\t");
+			*/
+			// cartList의 i번째 요소를 getter하여 
+			// 임시(for() 명령문 내에 있기때문에) 선언된
+			// cartVO 객체에 저장하라
+			CartVO cartVO = cartList.get(i);
+			this.printBody(cartVO);
+		}
+		System.out.println(Values.sLine);
 	}
 
 	@Override
 	public void printUserCart() {
-		// TODO 구매자별 장바구니 보기
+		System.out.println(Values.dLine);
+		System.out.println("구매자들 리스트");
+		System.out.print("구매자명 >> ");
+		String strUserName = scan.nextLine();
+		
+		this.printHeader();
+		int nSize = cartList.size();
+		for(int i = 0 ; i < nSize ; i++) {
+			CartVO cartVO = cartList.get(i);
+			if(cartVO.getUserName().equals(strUserName))
+			this.printBody(cartVO);
+		}
 		
 	}
 
