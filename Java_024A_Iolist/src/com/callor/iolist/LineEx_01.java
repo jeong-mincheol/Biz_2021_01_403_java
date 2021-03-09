b61ce5ec334e4f75d31e6072bd7e16e167a3ca45
jeong-mincheol <jmc5371@nate.com>
2021-03-09 12:34:38 +0900

    iolist

---
+++ Java_024A_Iolist/src/com/callor/iolist/LineEx_01.java
@@ -0,0 +1,14 @@
+package com.callor.iolist;
+
+import com.callor.iolist.values.Values;
+
+public class LineEx_01 {
+	
+	public static void main(String[] args) {
+		
+		System.out.println(Values.dLine(50));
+		System.out.println(Values.sLine(50));
+		
+	}
+
+}
